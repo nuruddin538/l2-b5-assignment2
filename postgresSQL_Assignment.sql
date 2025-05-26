d21ac279f81d44febaf2c92eebf2a79547e82c04
Nur Uddin <nuruddinnur538@gmail.com>
2025-05-26 12:09:06 +0600

    first commit

---
+++ postgresSQL_Assignment.sql
@@ -0,0 +1,7 @@
+-- Active: 1747475735074@@127.0.0.1@5432@conservation_db
+
+SELECT * FROM rangers;
+
+-- 1. Register new ranger
+
+INSERT INTO rangers (name, region) VALUES('Derek Fox', 'Coastal Plains');
